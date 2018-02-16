7ebc5fa61b19583040f04d8e16b0d386fd7141f3
Sam Meech-Ward <sam@meech-ward.com>
2018-02-16 07:47:51 -0800

    Create an transaction view controller to input transactions

---
+++ Budget/DataManager.swift
@@ -0,0 +1,34 @@
+// Copyright (c) 2017 Lighthouse Labs. All rights reserved.
+// 
+// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+// distribute, sublicense, create a derivative work, and/or sell copies of the
+// Software in any work that is designed, intended, or marketed for pedagogical or
+// instructional purposes related to programming, coding, application development,
+// or information technology.  Permission for such use, copying, modification,
+// merger, publication, distribution, sublicensing, creation of derivative works,
+// or sale is expressly withheld.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+class DataManager: NSObject {
+
+  let dailyBudget = NSDecimalNumber(string: "10.00")
+  var spent = NSDecimalNumber(string: "0.00")
+  
+  func budgetRemainingToday() -> NSDecimalNumber {
+    return dailyBudget.subtracting(spent)
+  }
+  
+  func spend(amount: NSDecimalNumber, time: Date) {
+    spent = spent.adding(amount)
+  }
+  
+}
